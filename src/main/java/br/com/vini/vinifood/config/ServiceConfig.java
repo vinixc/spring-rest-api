ca236e8b2dbb7e0546a4e75bc5a418bdd9aa0e02
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-08-28 20:02:38 -0300

    Criando métodos de callback do ciclo de vida dos beans

--- src/main/java/br/com/vini/vinifood/config/ServiceConfig.java
+++ src/main/java/br/com/vini/vinifood/config/ServiceConfig.java
@@ -1,13 +1,16 @@
 package br.com.vini.vinifood.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import br.com.vini.vinifood.service.AtivacaoClienteService;
 
 @Configuration
 public class ServiceConfig {
 	
-//	@Bean
-//	public AtivacaoClienteService ativacaoClienteService(Notificador notificador) {
-//		return new AtivacaoClienteService(notificador);
-//	}
+	@Bean(initMethod = "init", destroyMethod = "destroy")
+	public AtivacaoClienteService ativacaoClienteService() {
+		return new AtivacaoClienteService();
+	}
 
 }
--- src/main/java/br/com/vini/vinifood/service/AtivacaoClienteService.java
+++ src/main/java/br/com/vini/vinifood/service/AtivacaoClienteService.java
@@ -1,19 +1,28 @@
 package br.com.vini.vinifood.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import br.com.vini.vinifood.annotation.TipoDoNotificador;
 import br.com.vini.vinifood.enums.NivelUrgencia;
 import br.com.vini.vinifood.modelo.Cliente;
 import br.com.vini.vinifood.notificacao.Notificador;
 
-@Component
+//@Component
 public class AtivacaoClienteService {
 
 	@Autowired
 	@TipoDoNotificador(NivelUrgencia.NORMAL)
 	private Notificador notificador;
+	
+//	@PostConstruct
+	public void init() {
+		System.out.println("AtivacaoClienteService: INIT");
+	}
+	
+//	@PreDestroy
+	public void destroy() {
+		System.out.println("AtivacaoClienteService: DESTROY");
+	}
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
