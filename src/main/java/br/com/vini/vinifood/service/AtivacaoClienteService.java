dfbc18ef663e69d7c1b121486923be17ab7f4a6b
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-08-28 17:14:56 -0300

    Conhecendo os pontos de injeção e a anotação @Autowired

--- src/main/java/br/com/vini/vinifood/service/AtivacaoClienteService.java
+++ src/main/java/br/com/vini/vinifood/service/AtivacaoClienteService.java
@@ -1,16 +1,20 @@
 package br.com.vini.vinifood.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import br.com.vini.vinifood.modelo.Cliente;
 import br.com.vini.vinifood.notificacao.Notificador;
 
 public class AtivacaoClienteService {
 
+	@Autowired
 	private Notificador notificador;
 	
 	public AtivacaoClienteService(Notificador notificador) {
 		this.notificador = notificador;
-		
-		System.out.println("AtivacaoClienteService: " + notificador);
+	}
+	
+	public AtivacaoClienteService(String qualquer) {
 	}
 
 	public void ativar(Cliente cliente) {
