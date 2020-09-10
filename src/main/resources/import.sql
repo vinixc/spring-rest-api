44732c9643f8566af94c2937f181bcbc0e5894ca
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-09-09 23:11:46 -0300

    Mapeando relacionamento com @ManyToOne

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
-insert into cozinha (nome) values ('Tailandesa');
-insert into cozinha (nome) values ('Indiana');
+insert into cozinha (id,nome) values (1,'Tailandesa');
+insert into cozinha (id,nome) values (2,'Indiana');
+
+insert into restaurante (nome,taxa_frete,cozinha_id) values ('Tio ze', 10, 1);
+insert into restaurante (nome,taxa_frete,cozinha_id) values ('Bar do Peixe', 30, 2);
