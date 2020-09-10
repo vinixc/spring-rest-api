510754bfe47280cf6ea40c94330906cb76d006b0
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-09-09 22:47:18 -0300

    usando o Lombok

--- src/main/java/br/com/vini/vinifood/domain/model/Cozinha.java
+++ src/main/java/br/com/vini/vinifood/domain/model/Cozinha.java
@@ -8,67 +8,20 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 @Entity
 @Table(name = "cozinha")
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Cozinha implements Serializable{
 	private static final long serialVersionUID = 3084261461433261928L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
 	private Long id;
 	
 	private String nome;
-
-	public Cozinha() {}
-
-	public Cozinha(Long id, String nome) {
-		this.id = id;
-		this.nome = nome;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Cozinha other = (Cozinha) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "Cozinha [id=" + id + ", nome=" + nome + "]";
-	}
 }
